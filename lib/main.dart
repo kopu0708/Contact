875d13a1430805f56bf730780d15e63899aaff42
kopu0708 <ghop0822@gmail.com>
2025-08-26 18:51:49 +0900

    두번째 커밋

--- lib/main.dart
+++ lib/main.dart
@@ -1,39 +1,43 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget{
-  const MyApp({Key? key}) : super(key: key);
+ MyApp({Key? key}) : super(key: key);
+
+   var a = 1;
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
         home: Scaffold(
-          appBar: AppBar(
+          floatingActionButton: FloatingActionButton(
+              child: Text(a.toString()),
+              onPressed: (){
+                print (a);
+                a++;
+              }
+          ),
+          appBar: AppBar( title: Align(
+            alignment: Alignment. bottomRight,
+            child: Container(child:Text('dkdk'),width:50,height: 50,),
+          ),
             backgroundColor: Color(0xFF4287F8),
           ),
           body:
           Padding(
             padding: const EdgeInsets.all(8.0),
-            child: ListView(
-                    children: [
-                         Row(
-                          children: [
-                            Icon(Icons.account_circle,size:70),
-                            SizedBox(width: 5,),
-                            Text('홍길동',style: TextStyle(fontSize: 24.0),)
-                          ],
-                        ),
-                      SizedBox(height: 10,),
-                      Row(
-                        children: [
-                          Icon(Icons.account_circle,size:70),
-                          SizedBox(width: 5,),
-                          Text('홍길동',style: TextStyle(fontSize: 24.0))
-                        ],
-                      )
-                    ],
+            child: ListView.builder(
+               itemCount: 100,
+               itemBuilder: (context, i){
+                 print(i);
+                 return ListTile(
+                   leading: Icon(Icons.account_circle),
+                   title: Text('user'),
+                 );
+               },
             ),
           ),
          bottomNavigationBar: BottomAppBar(
