0b2b4d26a87572b672cadc34204e6b6d785be538
kopu0708 <ghop0822@gmail.com>
2025-09-01 17:09:56 +0900

    자식 statuse를 부모로 올리는 연습을 하는 숙제를 하였다.

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,6 @@ void main() {
   );
 }
 
-
 class MyApp extends StatefulWidget{
  MyApp({Key? key}) : super(key: key);
 
@@ -19,27 +18,67 @@ class MyApp extends StatefulWidget{
 }
 
 class _MyAppState extends State<MyApp> {
+  int a = 0;
+  var friends = ['이정철','홍길동','아무개'];
+  var like = [0,0,0];
+
+  void increaseA(){
+    setState(() {
+      a++;
+    });
+  }
   @override
   Widget build(BuildContext context) {
 
     return Scaffold(
-        floatingActionButton: FloatingActionButton(
-          onPressed: (){
-            showDialog(context: context, builder: (context){
-              return Dialog(child: Text('안녕'));
-            });
-          },
+        appBar: AppBar(),
+        body: Column(
+          children: [
+            SizedBox(child: Text(a.toString()),),
+            Expanded(
+              child: ListView.builder(
+                itemCount: 3,
+                itemBuilder: (c,i){
+                  return
+                    ListTile(
+                    leading: Icon(Icons.account_circle),
+                    title: Text(friends[i].toString()),
+                  );
+                }),
+            ),
+          ],
         ),
-            appBar: AppBar(),
-      body: ListView.builder(
-          itemCount: 3,
-          itemBuilder: (c,i){
-            return ListTile(
-              leading: Icon(Icons.account_circle),
-              title: Text('user'),
-            );
-          })
+      floatingActionButton: FloatingActionButton(
+        onPressed: (){
+           showDialog(context: context, builder: (context){
+        return DialogUI(state : a, increaseA: increaseA);
+      });
+    }
+    ),
+    );
+  }
+}
 
+class DialogUI extends StatelessWidget {
+  const DialogUI({super.key,this.state, required this.increaseA});
+    final state;
+    final Function() increaseA;
+  @override
+  Widget build(BuildContext context) {
+    return Dialog(
+      child: Container(
+        width: 300,
+        height: 300,
+        child: Column(
+          children: [
+            TextField(),
+            TextButton(onPressed: (){increaseA();}, child: Text('a를 늘려주기')),
+            TextButton(onPressed: (){Navigator.pop(context);},
+                       child: Text('취소')),
+            Text(state.toString())
+          ],
+        ),
+      ),
     );
   }
 }
