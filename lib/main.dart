85c68b25cc1ad1e52271d7977facd78b8c79741e
이정철 <ghop0822@gmail.com>
2025-09-02 14:29:47 +0900

    Update main.dart
    리스트 추가 및 삭제 기능, 리스트 정렬 기능, 빈칸 입력 시 리스트 추가가 되는 것을 막음 ---09/02

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+
 import 'dart:typed_data';
 
 import 'package:flutter/material.dart';
@@ -18,13 +19,30 @@ class MyApp extends StatefulWidget{
 }
 
 class _MyAppState extends State<MyApp> {
-  int a = 0;
-  var friends = ['이정철','홍길동','아무개'];
-  var like = [0,0,0];
+  var total = 0;
+  var friends = [];
+  var inputData = TextEditingController();//입력값 저장
 
-  void increaseA(){
+
+  void increaseA() { // 친구추가 함수
+    if (inputData.text.isEmpty) {
+      print('입력된 내용이 없습니다.'); // 사용자에게 알려주거나 로그 출력
+      return; // 여기서 함수를 조기 종료
+    }
     setState(() {
-      a++;
+      friends.add(inputData.text);
+      total++;
+      Navigator.pop(context); // Dialog를 닫는 코드
+    });
+
+    // Dialog에서 추가 버튼을 누른 후 입력창을 비워준다.
+    inputData.clear();
+  }
+
+  void DelectFriends(int index){//친구삭제 함수
+    setState(() {
+      friends.removeAt(index);
+      total--;
     });
   }
   @override
@@ -34,25 +52,32 @@ class _MyAppState extends State<MyApp> {
         appBar: AppBar(),
         body: Column(
           children: [
-            SizedBox(child: Text(a.toString()),),
+            SizedBox(child: Text(total.toString()),),
             Expanded(
               child: ListView.builder(
-                itemCount: 3,
+                itemCount: friends.length,
                 itemBuilder: (c,i){
                   return
                     ListTile(
                     leading: Icon(Icons.account_circle),
                     title: Text(friends[i].toString()),
+                      trailing: IconButton(onPressed: (){DelectFriends(i);},
+                          icon: Icon(Icons.cancel))
                   );
                 }),
             ),
           ],
         ),
+      bottomNavigationBar: Container(width: 300, height: 100,color: Colors.blue,
+                           child: ElevatedButton(onPressed: (){setState(() {
+                             friends.sort();
+                           });}, child: Text('정렬'),),
+                           ),
       floatingActionButton: FloatingActionButton(
         onPressed: (){
            showDialog(context: context, builder: (context){
-        return DialogUI(state : a, increaseA: increaseA);
-      });
+        return DialogUI(state : total, increaseA: increaseA, inputData:inputData);
+      });//자식 위젯에 전송하고
     }
     ),
     );
@@ -60,9 +85,11 @@ class _MyAppState extends State<MyApp> {
 }
 
 class DialogUI extends StatelessWidget {
-  const DialogUI({super.key,this.state, required this.increaseA});
+   DialogUI({super.key,this.state, required this.increaseA, this.inputData});
     final state;
     final Function() increaseA;
+    var inputData;
+
   @override
   Widget build(BuildContext context) {
     return Dialog(
@@ -71,16 +98,13 @@ class DialogUI extends StatelessWidget {
         height: 300,
         child: Column(
           children: [
-            TextField(),
-            TextButton(onPressed: (){increaseA();}, child: Text('a를 늘려주기')),
-            TextButton(onPressed: (){Navigator.pop(context);},
+            TextField(controller: inputData),
+            TextButton(onPressed: (){increaseA();}, child: Text('친구추가')),//버튼을 누르면 함수호출
+            TextButton(onPressed: (){Navigator.pop(context);},//Dialog창 닫기 버튼
                        child: Text('취소')),
-            Text(state.toString())
           ],
         ),
       ),
     );
   }
 }
-
-
